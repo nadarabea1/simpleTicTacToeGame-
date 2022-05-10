65d073ecb02ea58ee7bb410042d5909e5a50c7cf
nadarabea1 <nadarabea81@gmail.com>
2022-05-10 23:09:30 +0200

    Play Method

--- src/tic/tac/toe/Board.java
+++ src/tic/tac/toe/Board.java
@@ -1,15 +1,15 @@
-
 package tic.tac.toe;
 
 public class Board {
-    private final int ROWS;   
+
+    private final int ROWS;
     private final int COLNS;
-    private Shapes [][] game;
-    
-    public Board(int rows, int colns){
-        this.ROWS=rows;
-        this.COLNS=colns;
-        game=new Shapes[rows][colns];
+    private Shapes[][] game;
+
+    public Board(int rows, int colns) {
+        this.ROWS = rows;
+        this.COLNS = colns;
+        game = new Shapes[rows][colns];
     }
 
     public int getROWS() {
@@ -19,30 +19,36 @@ public class Board {
     public int getCOLNS() {
         return COLNS;
     }
-    
-    public boolean isFree(int row, int coln){
-        boolean retavl=false;
-        try{
-        if(game[row][coln]!= null){
-            retavl =true;
-        }
-        }
-        catch(ArrayIndexOutOfBoundsException ex){
-            
+
+    public boolean isFree(int row, int coln) {
+        boolean retavl = false;
+        try {
+            if (game[row][coln] != null) {
+                retavl = true;
+            }
+        } catch (ArrayIndexOutOfBoundsException ex) {
+
         }
         return retavl;
 
     }
+
+    public boolean play(String name, int row, int coln) {
+        boolean retval = false;
+        if (isFree(row, coln)) {
+            game[row][coln] = new Shapes(name, row, coln);
+        }
+        return retval;
+    }
+
     
-   
-    
-    public void print(){
-        for(int rows=0; rows< ROWS; rows++){
-            for(int colns=0; colns< COLNS; colns++){
-                if(game[rows][colns]==null){
+
+    public void print() {
+        for (int rows = 0; rows < ROWS; rows++) {
+            for (int colns = 0; colns < COLNS; colns++) {
+                if (game[rows][colns] == null) {
                     System.out.print("NO ");
-                }
-                else{
+                } else {
                     System.out.print(game[rows][colns].getName());
 
                 }
@@ -51,8 +57,5 @@ public class Board {
 
         }
     }
-    
-    
-    
 
 }
