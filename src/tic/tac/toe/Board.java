07fff05fb944a4a1bd31a0d1a77f365bd2472362
nadarabea1 <nadarabea81@gmail.com>
2022-05-11 22:35:43 +0200

    initial haveWinner Method

--- src/tic/tac/toe/Board.java
+++ src/tic/tac/toe/Board.java
@@ -38,6 +38,7 @@ public class Board {
         if (isFree(row, coln)) {
             game[row][coln] = new Squares(name, row, coln);
         }
+        print();
         return retval;
     }
 
@@ -51,6 +52,7 @@ public class Board {
                 }
             }
         }
+        print();
         return retval;
     }
 
@@ -58,7 +60,7 @@ public class Board {
         for (int rows = 0; rows < ROWS; rows++) {
             for (int colns = 0; colns < COLNS; colns++) {
                 if (game[rows][colns] == null) {
-                    System.out.print("NO ");
+                    System.out.print(" ");
                 } else {
                     System.out.print(game[rows][colns].getName());
 
@@ -75,6 +77,26 @@ public class Board {
             for(int colns=0; colns<COLNS; colns++){
                 if(game[rows][colns]==null)return true;
             }
+        }
+        return retval;
+    }
+    public boolean haveWinner(){
+        boolean retval=false;
+        int count=0;
+        for(int rows=0; rows<ROWS; rows++){
+            for(int colns=0 , near=1; colns<COLNS-1; colns++, near++){
+                if(game[rows][colns].getName()==game[rows][near].getName())count++;
+            }
+            if(count==2)return true;
+            count=0;
+        }
+        
+        for(int colns=0; colns<COLNS; colns++){
+            for(int rows=0 , near=1; rows<ROWS-1; rows++, near++){
+                if(game[rows][colns].getName()==game[near][colns].getName())count++;
+            }
+            if(count==2)return true;
+            count=0;
         }
         return retval;
     }
--- src/tic/tac/toe/TicTacToe.java
+++ src/tic/tac/toe/TicTacToe.java
@@ -11,24 +11,27 @@ public class TicTacToe {
         Board board = new Board(3, 3);
 
         board.print();
-        char turn = palyer1;
-        boolean stop = false;
-        while (!stop) {
-            boolean vaildPlay=true;
-            if (turn == palyer1) {
-                do{
-                System.out.print("Rows - Colmns: ");
-                int row = sc.nextInt();
-                int coln = sc.nextInt();
-                if (board.isFree(row, coln)) {
-                    board.play(turn, row, coln);
-                }
-                else vaildPlay=false;
-                }
-                while(!vaildPlay);
+        System.out.println(board.haveWinner());
+//        char turn = palyer1;
+//        boolean stop = false;
+//        while (!stop) {
+//            boolean vaildPlay=true;
+//            if (turn == palyer1) {
+//                do{
+//                System.out.print("Rows - Colmns: ");
+//                int row = sc.nextInt();
+//                int coln = sc.nextInt();
+//                if (board.isFree(row, coln)) {
+//                    board.play(turn, row, coln);
+//                }
+//                else vaildPlay=false;
+//                }
+//                while(!vaildPlay);
+//
+//            }
+//        }
 
-            }
-        }
+
     }
 
 }
